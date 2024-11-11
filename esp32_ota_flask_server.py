ef6c25a209d94d9de14d1be95f05244072d4f697
felipe_queo <felipe.oliveira@queonetics.com>
2024-11-10 22:52:06 -0300

    Initial commit

---
+++ esp32_ota_flask_server.py
@@ -0,0 +1,19 @@
+from flask import Flask, send_file, request
+
+app = Flask(__name__)
+
+@app.route('/get/firmware')
+def api_firmware_data():
+    try:
+        return send_file('embarcados_dota_fw1_to_embarcados_dota_fw2.bin')
+    except Exception as e:
+        return str(e)
+
+
+@app.route('/')
+def hello_word():
+    return "<p>Esp32 OTA Server running on: " + request.host + " !</p>"
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=9001, debug=True, threaded=True)
